4898e9af36d0573f38719cf83203025075020f5e
Hazel Victoria Campbell <hazel.campbell@ualberta.ca>
2023-09-21 18:03:34 -0600

    Add forms

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const http = require("http"); // node!
 const assert = require('node:assert').strict;
 
 const html_entities = require('html-entities');
-const encode = html_entities.encode;
 const nunjucks = require("nunjucks");
+const multipart = require("parse-multipart-data");
+const BufferHex = require("buffer-hex").BufferHex;
 
-const host = 'localhost';
+const host = '';
 const port = 9000;
 
 const split1 = function (str, sep) {
@@ -60,78 +61,149 @@ const fix6 = function (addr) {
     }
 }
 
+const params2Aarray = function(params) {
+    const r = [];
+    for (const [name, value] of params) {
+        r.push({
+            name: name,
+            value: value,
+        })
+    }
+    return r;
+}
+
 const requestListener = function (req, res) {
-    const context = {};
-    const references = [];
-    const breakCircularReferences = function (key, value) {
-        let found = false;
-        if (
-            value === null
-            || value === undefined
-            || !Object.keys(value).length
-        ) {
-            return value;
-        }
-        for (reference of references) {
-            if (Object.is(value, reference)) {
-                found = true;
+    let read = 0;
+    let chunks = [];
+    let tooBig = false;
+    req.on('readable', () => {
+        let chunk;
+        while (null != (chunk = req.read(128))) {
+            chunks.push(chunk);
+            read += chunk.length;
+            if (read > 1024) {
+                tooBig = true;
+                req.end();
             }
         }
-        if (found) {
-            return "...";
+    });
+    req.on('end', () => {
+        const context = {
+            read: read,
+            body: chunks.join(''),
+        };
+        chunks = Buffer.concat(chunks);
+        const references = [];
+        const breakCircularReferences = function (key, value) {
+            let found = false;
+            if (
+                value === null
+                || value === undefined
+                || !Object.keys(value).length
+            ) {
+                return value;
+            }
+            for (reference of references) {
+                if (Object.is(value, reference)) {
+                    found = true;
+                }
+            }
+            if (found) {
+                return "...";
+            } else {
+                references.push(value);
+                return value;
+            }
+        }
+        context.request_string = JSON.stringify(req, breakCircularReferences, '  ');
+        const url = new URL(req.url, `http://${req.headers.host}`);
+        context.url = '' + url;
+        res.setHeader('Content-Type', 'text/html');
+        if (tooBig) {
+            res.writeHead(413, "Request Entity Too Large");
+        } else if (url.pathname.includes("/basic") && !req.headers['authorization']) {
+            res.writeHead(401, "Unauthorized", {
+                'WWW-Authenticate': 'Basic realm="Enter any username and password"',
+            });
         } else {
-            references.push(value);
-            return value;
+            res.writeHead(200, "OK", {
+            });
         }
-    }
-    context.request_string = JSON.stringify(req, breakCircularReferences, '  ');
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    context.url = '' + url;
-    res.setHeader('Content-Type', 'text/html');
-    if (url.pathname.includes("/basic") && !req.headers['authorization']) {
-        res.writeHead(401, "Unauthorized", {
-            'WWW-Authenticate': 'Basic realm="Enter any username and password"',
-        });
-    } else {
-        res.writeHead(200, "OK", {
-        });
-    }
-    context.headers_string = JSON.stringify(req.headers, undefined, '  ');
-    context.search_params = JSON.stringify(Object.fromEntries(url.searchParams), undefined, '  ');
-    context.from_host = fix6(req.socket.remoteAddress);
-    context.from_port = req.socket.remotePort;
-    context.to_host = fix6(req.socket.localAddress);
-    context.to_port = req.socket.localPort;
-    context.version = req.httpVersion;
-    context.method = req.method;
-    context.path = req.url;
-    context.protocol = url.protocol;
-    context.username = url.username;
-    context.password = url.password;
-    context.host = url.host;
-    context.hostname = url.hostname;
-    context.port = url.port;
-    context.pathname = url.pathname;
-    context.search = url.search;
-    context.hash = url.hash;
-    context.origin = url.origin;
-    if (req.headers['authorization']) {
-        let auth = req.headers['authorization'];
-        let [auth_kind, auth_value] = split1(req.headers.authorization, " ");
-        context.auth_kind = auth_kind;
-        context.auth_value = auth_value;
-        if (noCaseCompare(auth_kind, "Basic")) {
-            context.basic = Buffer.from(auth_value, 'base64').toString();
-            [context.user, context.password] = split1(context.basic, ":");
+        context.statusCode = res.statusCode;
+        context.statusMessage = res.statusMessage;
+        context.headers_string = JSON.stringify(req.headers, undefined, '  ');
+        context.search_params = JSON.stringify(Object.fromEntries(url.searchParams), undefined, '  ');
+        context.from_host = fix6(req.socket.remoteAddress);
+        context.from_port = req.socket.remotePort;
+        context.to_host = fix6(req.socket.localAddress);
+        context.to_port = req.socket.localPort;
+        context.version = req.httpVersion;
+        context.method = req.method;
+        context.path = req.url;
+        context.protocol = url.protocol;
+        context.username = url.username;
+        context.password = url.password;
+        context.host = url.host;
+        context.hostname = url.hostname;
+        context.port = url.port;
+        context.pathname = url.pathname;
+        context.search = url.search;
+        context.hash = url.hash;
+        context.origin = url.origin;
+        if (req.headers['authorization']) {
+            let auth = req.headers['authorization'];
+            let [auth_kind, auth_value] = split1(req.headers.authorization, " ");
+            context.auth_kind = auth_kind;
+            context.auth_value = auth_value;
+            if (noCaseCompare(auth_kind, "Basic")) {
+                context.basic = Buffer.from(auth_value, 'base64').toString();
+                [context.user, context.password] = split1(context.basic, ":");
+            }
         }
-    }
-    res.end(
-        nunjucks.render("index.njk", context)
-    );
+        if (req.headers['content-type']) {
+            const [contentType, ctArg] = split1(req.headers['content-type'], ";");
+            context.contentType = contentType;
+            context.ctArg = ctArg;
+            if (noCaseCompare(contentType, "application/x-www-form-urlencoded")) {
+                try {
+                    context.postData = params2Aarray(new URLSearchParams(context.body));
+                } catch (err) {
+                    context.postError = '' + err;
+                }
+            } else if (noCaseCompare(contentType, "multipart/form-data")) {
+                const [ctArgKey, ctArgVal] = split1(ctArg, "=");
+                context.boundary = false;
+                if (ctArgKey.includes("boundary")) {
+                    context.boundary = ctArgVal;
+                }
+                if (context.boundary) {
+                    try {
+                        const parts = multipart.parse(chunks, context.boundary);
+                        context.parts = parts.length;
+                        for (part of parts) {
+                            if (Buffer.isBuffer(part.data)) {
+                                part.data = BufferHex.dump(part.data);
+                            }
+                        }
+                        context.postData = parts;
+                    } catch (err) {
+                        context.postError = '' + err;
+                    }
+                } else {
+                    context.postError = "Couldn't find the boundary? " + ctArgKey + "..." + ctArgVal
+                }
+            }
+        }
+        if (url.pathname.includes("/form")) {
+            res.end(nunjucks.render("form.njk", context));
+        } else {
+            res.end(nunjucks.render("index.njk", context));
+        }
+    });
 };
 
 const main = function () {
-    nunjucks.configure({ 
+    nunjucks.configure({
         autoescape: true,
         noCache: true,
     });
